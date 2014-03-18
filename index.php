51d1cc7389ba4d3c27b884e86bf367ac7c6b2360
juanpreciado <juan.preciado@gmail.com>
2014-03-18 16:11:43 -0500

    Se sube poroyecto

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+    <head>
+        <link rel="stylesheet/less" type="text/css" href="style.less" />
+        <script src="less.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <div class="shape" id="shape1"></div>
+        <div class="shape" id="shape2"></div>
+        <div class="shape" id="shape3"></div>
+    </body>
+</html>
